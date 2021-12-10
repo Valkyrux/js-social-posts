1d906d4eddc4780872a371f8492204a1268ffe59
Valkyrux <philadephos.granata@gmail.com>
2021-12-10 15:53:39 +0100

    init

---
+++ main.js
@@ -0,0 +1,10 @@
+// creo un array di oggetti che contengono tutte le informazioni per generare un post
+const arrayPosts = [
+    {
+        author: "Phil Mangione",
+        time: "4 mesi fa",
+        text: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        image: 171,
+        likecounter: 80
+    }
+]
