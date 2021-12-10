acd85d3821f035043699d021c7fe19f96fa4d681
Valkyrux <philadephos.granata@gmail.com>
2021-12-10 16:14:33 +0100

    print-object-by-js

--- main.js
+++ main.js
@@ -2,9 +2,51 @@
 const arrayPosts = [
     {
         author: "Phil Mangione",
+        profilepic: 15,
         time: "4 mesi fa",
         text: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
         image: 171,
         likecounter: 80
     }
 ]
+// funzione che stampa i post prelevando i dati da arrayPosts
+function printObjectsOnHTML(objectArray, positionOnDOM) {
+    for (let i = 0; i < objectArray.length; i++) {
+        positionOnDOM.innerHTML += `
+        <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="https://unsplash.it/300/300?image=${objectArray[i].profilepic}" alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${objectArray[i].author}</div>
+                    <div class="post-meta__time">${objectArray[i].time}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+        <div class="post__image">
+            <img src="https://unsplash.it/600/300?image=${objectArray[i].image}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-${i + 1}" class="js-likes-counter">${objectArray[i].likecounter}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>`
+        
+    }
+}
+// prelevo la posizione dal DOM
+const container = document.getElementById("container");
+
+printObjectsOnHTML(arrayPosts, container);
